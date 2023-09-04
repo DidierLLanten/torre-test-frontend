7b585339c8c1f2d8e812d68f405896ed1b7bee30
DidierLLantenPc <didiervelez@hotmail.es>
2023-09-04 12:50:15 -0500

    Add toolbar

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [MatToolbarModule],
+  exports: [
+    MatToolbarModule, 
+    MatIconModule
+  ],
 })
 export class MaterialModule {}
